346d288e68485ae0100b2d4e3172b774a921ba58
Дмитрий Стародубов <dmitrii.starodubov@fogstream.ru>
2020-01-27 12:50:49 +1000

    ~ fixed static files dir

--- findyourhappiness/settings.py
+++ findyourhappiness/settings.py
@@ -104,10 +104,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static_dev'),
 ]
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 
